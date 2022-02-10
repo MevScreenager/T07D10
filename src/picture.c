733814a21bba02d021d66a48fdd2ceee1779109d
Arryn Pallas <arrynpal@ox-t3.msk.21-school.ru>
2022-02-10 22:45:08 +0300

    i

--- src/picture.c
+++ src/picture.c
@@ -1,57 +1,39 @@
+#include<stdio.h>
 #define N 15
 #define M 13
 
-void transform(int *buf, int **matr, int n, int m);
+void transform(int **matr);
 void make_picture(int **picture, int n, int m);
-void reset_picture(int **picture, int n, int m);
 
-void main()
-{
-   int picture_data[N][M];
-   int *picture[N];
-   transform(picture_data, picture, N, M);
+int main() {
+    int picture_data[N][M] = { { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                              { 1, 0, 0, 0, 0, 0, 1, 0, 6, 6, 6, 6, 1 },
+                              { 1, 0, 0, 3, 3, 0, 1, 0, 0, 6, 6, 6, 1 },
+                              { 1, 0, 3, 3, 3, 3, 1, 0, 0, 6, 6, 6, 1 },
+                              { 1, 0, 3, 3, 3, 3, 1, 0, 6, 0, 0, 6, 1 },
+                              { 1, 0, 0, 3, 3, 0, 1, 0, 0, 0, 0, 0, 1 },
+                              { 1, 0, 0, 7, 7, 0, 1, 0, 0, 0, 0, 0, 1 },
+                              { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                              { 1, 0, 0, 7, 7, 0, 1, 0, 0, 0, 0, 0, 1 },
+                              { 1, 0, 0, 7, 7, 0, 1, 0, 0, 0, 0, 0, 1 },
+                              { 1, 0, 7, 7, 7, 7, 1, 0, 0, 0, 0, 0, 1 },
+                              { 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1 },
+                              { 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1 },
+                              { 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1 },
+                              { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 } };
 
-   make_picture(picture, );
-}
-
-void make_picture(int **picture, int n, int m)
-{
-   int frame_w[] = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
-   int frame_h[] = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
-   int tree_trunk[] = { 7, 7, 7, 7 };
-   int tree_foliage[] = { 3, 3, 3, 3 };
-   int sun_data[6][5] = { { 0, 6, 6, 6, 6 }, 
-                          { 0, 0, 6, 6, 6 }, 
-                          { 0, 0, 6, 6, 6 },
-                          { 0, 6, 0, 0, 6 },
-                          { 0, 0, 0, 0, 0 },
-                          { 0, 0, 0, 0, 0 } };
-
-   reset_picture(picture, n, m);
-
-   int length_frame_w = sizeof(frame_w) / sizeof(frame_w[0]);
-   
-   for (int i = 0; i < length_frame_w; i++)
-   {
-      picture[0][i] = frame_w[i];
-   }
-}
-
-void reset_picture(int **picture, int n, int m)
-{
-   for (int i = 0; i < m; i++)
-   {
-      for (int j = 0; j < n; j++)
-      {
-         picture[i][j] = 0;
-      }  
-   }
-}
-
-void transform(int *buf, int **matr, int n, int m)
-{  
-   for(int i = 0; i < n; i++)
-   {
-      matr[i] = buf + i * m;
-   }
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < M; j++) {
+         if (i == N - 1 && j == M - 1) {
+            printf("%d", picture_data[i][j]);
+            break;
+         }
+         if (j == M - 1) {
+            printf("%d\n", picture_data[i][j]);
+            break;
+         }
+         printf("%d ", picture_data[i][j]);
+      }
+    }
+    return 0;
 }
